83c6e22c04a0c443e0930d5401820909966afbe2
Anubhab De <154841713+anubhabde07@users.noreply.github.com>
2026-02-23 01:23:03 +0530

    Update streamlit_app.py

--- YouTube_Video/streamlit_app.py
+++ YouTube_Video/streamlit_app.py
@@ -7,13 +7,14 @@ from utils.url_to_id import get_videoID
 st.title("YouTube Transcript ChatBot")
 
 video_url = st.text_input("Enter YouTube Video URL")
+api_key = st.text_input("Enter api key")
 
 video_id = ""
 if video_url:
     video_id += get_videoID(video_url)
 
-if video_id:
-    transcript = get_transcript(video_id)
+if video_id & api_key:
+    transcript = get_youtube_transcript(video_id, api_key)
 
     if not transcript:
         st.error("Transcript could not be fetched.")
@@ -28,3 +29,4 @@ if video_id:
             answer = chain.invoke(query)
 
             st.write(answer)
+
