4c3c0d0f23c7f7a48cf72a73e4c1803cd51fae1a
Anubhab De <154841713+anubhabde07@users.noreply.github.com>
2026-02-22 20:01:58 +0530

    Update transcript.py

--- YouTube_Video/utils/transcript.py
+++ YouTube_Video/utils/transcript.py
@@ -1,10 +1,23 @@
-from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled
+from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api._errors import (
+    TranscriptsDisabled,
+    RequestBlocked,
+    NoTranscriptFound
+)
 
 def get_transcript(video_id: str) -> str:
     try:
         transcript_obj = YouTubeTranscriptApi()
-        fetched_transcript = transcript_obj.fetch(video_id=video_id, languages=['en'])
-        transcript_list = [snippet.text for snippet in fetched_transcript.snippets]
+        fetched_transcript = transcript_obj.fetch(
+            video_id=video_id,
+            languages=['en']
+        )
+
+        transcript_list = [
+            snippet.text for snippet in fetched_transcript.snippets
+        ]
+
         return " ".join(transcript_list)
-    except TranscriptsDisabled:
+
+    except (TranscriptsDisabled, RequestBlocked, NoTranscriptFound):
         return ""
