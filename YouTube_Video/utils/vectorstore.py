36d74bfa5e7f9bd4ee5e7ac48aea9ecd5258d252
Anubhab De <154841713+anubhabde07@users.noreply.github.com>
2026-02-23 02:05:33 +0530

    Update vectorstore.py

--- YouTube_Video/utils/vectorstore.py
+++ YouTube_Video/utils/vectorstore.py
@@ -1,21 +1,28 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceInferenceAPIEmbeddings
+import streamlit as st
+
 
 def create_vectorstore(transcript: str):
+
     splitter = RecursiveCharacterTextSplitter(
-	    chunk_size=700,
-	    chunk_overlap=170,
+        chunk_size=700,
+        chunk_overlap=170,
     )
 
     chunks = splitter.create_documents([transcript])
 
-    embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2", model_kwargs={"device": "cpu"})
+    hf_token = st.secrets["HUGGINGFACEHUB_API_TOKEN"]
 
-    vector_store = FAISS.from_documents(
-	    documents=chunks,
-	    embedding=embedding
+    embedding = HuggingFaceInferenceAPIEmbeddings(
+        api_key=hf_token,
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
     )
-
+	
+    vector_store = FAISS.from_documents(
+        documents=chunks,
+        embedding=embedding
+    )
 
     return vector_store
